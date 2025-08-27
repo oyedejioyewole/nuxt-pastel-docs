0761555037626fddf1eda192f6bb63f9c20cc052
oyedejioyewole <57059138+oyedejioyewole@users.noreply.github.com>
2025-08-27 10:08:49 +0100

    chore: 🔧 Move layer modules to dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,41 @@
 export default defineNuxtConfig({
   modules: [
-    '@formkit/auto-animate/nuxt',
-    '@nuxt/content',
-    '@nuxt/fonts',
-    '@nuxt/eslint',
-    '@nuxtjs/color-mode',
-    '@nuxtjs/tailwindcss',
-    '@vueuse/nuxt',
-    'nuxt-og-image',
-    process.env.ICON_MODULE ?? 'nuxt-phosphor-icons',
+    "@formkit/auto-animate/nuxt",
+    "@nuxt/content",
+    "@nuxt/fonts",
+    "@nuxt/eslint",
+    "@nuxtjs/color-mode",
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "nuxt-og-image",
+    process.env.ICON_MODULE ?? "nuxt-phosphor-icons",
   ],
   devtools: { enabled: true },
   app: {
     head: {
-      link: [
-        { rel: 'icon', type: 'image/svg+xml', href: './favicon.svg' },
-      ],
+      link: [{ rel: "icon", type: "image/svg+xml", href: "./favicon.svg" }],
     },
   },
   colorMode: {
-    classSuffix: '',
+    classSuffix: "",
   },
   content: {
     build: {
       markdown: {
         highlight: {
           theme: {
-            dark: 'github-dark',
-            default: 'github-light',
+            dark: "github-dark",
+            default: "github-light",
           },
         },
       },
     },
   },
-  compatibilityDate: '2025-08-02',
+  compatibilityDate: "2025-08-02",
   ogImage: {
     zeroRuntime: true,
   },
   phosphorIcons: {
-    componentName: 'ui-icon',
+    componentName: "ui-icon",
   },
-})
+});
